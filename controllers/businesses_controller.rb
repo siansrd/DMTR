6d3d8779030a2b4a0529b28784a97496585cb0ec
siansrd <sianrobinsondavies@gmail.com>
2016-08-13 12:31:52 +0100

    Add business.save and new business form

--- controllers/businesses_controller.rb
+++ controllers/businesses_controller.rb
@@ -1,3 +1,11 @@
+require_relative('../models/business')
+
 get ('/new') do
   erb(:'businesses/new')
+end
+
+post ('/index') do
+ business = Business.new(params)
+ business.save
+ redirect to( '/index' )
 end
--- models/business.rb
+++ models/business.rb
@@ -1,18 +1,18 @@
 require('pg')
-require('../db/sql_runner')
+require_relative('../db/sql_runner')
 
 class Business
 
   attr_reader :name
 
-  def initilize(id, name)
-    @id = id.to_i
-    @name = name
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
   end
 
   def save()
     sql = "INSERT INTO businesses (name) 
-    VALUES '#{@name}'
+    VALUES ('#{@name}')
     RETURNING *;"
     business = SqlRunner.run( sql ).first
     @id = business['id']
