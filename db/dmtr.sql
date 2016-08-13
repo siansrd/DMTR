3e5141628cfe7b54e9f84580e689b9ef7c2298a9
siansrd <sianrobinsondavies@gmail.com>
2016-08-13 10:36:00 +0100

    Add tables

---
+++ db/dmtr.sql
@@ -0,0 +1,25 @@
+DROP TABLE businesses;
+DROP TABLE categories;
+DROP TABLE transactions;
+
+CREATE TABLE businesses (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE categories (
+  id SERIAL8 PRIMARY KEY,
+  label VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  day DATE,
+  amount DECIMAL(6,2),
+  supplier VARCHAR(255),
+  business_id references businesses(id),
+  category_id references categories(id)
+);
+
+
+
