d8d5362257822ea63c8cf364e3a78de0e54c054c
siansrd <sianrobinsondavies@gmail.com>
2016-08-15 13:25:40 +0100

    Add an analysis models and spec files.

--- models/analysis.rb
+++ models/analysis.rb
@@ -1,4 +1,31 @@
+require_relative('./business')
+require_relative('./category')
+require_relative('./transaction')
+require('pry-byebug')
+
+
+
 class Analysis
+
+  attr_reader :categories, :businesses, :transactions
+
+  def initialize(options)
+    @categories     = options['categories']
+    @businesses     = options['businesses']
+    @transactions   = options['transactions']
+  end
+
+  def businesses_names()
+    business_names = []
+    @businesses.each do |business|
+      business_names << business.name
+    end
+    return business_names
+  end
+
+  def business_count()
+    @businesses.count
+  end
 
 
 end
---
+++ specs/analysis_spec.rb
@@ -0,0 +1,33 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../models/analysis')
+require_relative('../models/category')
+require_relative('../models/business')
+
+class TestAnalysis < Minitest::Test
+
+  def setup
+    @test_categories_array   = Category.all
+    @test_businesses_array   = Business.all
+    @test_transactions_array = Transaction.all
+
+    test_analysis_data = { 
+      'categories'    => @test_categories_array, 
+      'businesses'    => @test_businesses_array, 
+      'transactions'  => @test_transactions_array 
+    }
+
+    @analysis = Analysis.new(test_analysis_data)
+  end
+
+
+  def test_businesses_names
+    assert_equal(['Web Design', 'Teaching'], @analysis.businesses_names)
+  end
+
+  def test_businesses_count
+    assert_equal(2, @analysis.business_count)
+  end
+
+
+end
